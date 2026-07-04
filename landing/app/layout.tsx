697493754485eba193a65ceff72a8235c465dfbd
mdraseem <mdraseeem@gmail.com>
2026-07-05 00:25:06 +1000

    update email fix

--- landing/app/layout.tsx
+++ landing/app/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head />
-      <body className="antialiated dark">
+      <body className="antialiased dark">
         <GoogleAnalytics />
         {children}
       </body>
